07bf4b302f4aca052e9a4bc327ad1143e5a72c05
Michail Votis <mikevotis67@gmail.com>
2026-04-05 14:25:25 +0300

    Add turtle.catch.py - Turtle catching game

---
+++ turtle.catch.py
@@ -0,0 +1,36 @@
+from turtle import *
+from time import sleep
+from random import randint
+
+w = 200
+h = 150
+
+t = Turtle()
+t.color("red")
+t.shape("turtle")
+t.penup()
+t.speed(0)
+t.points = 0
+
+
+def rand_move():
+    t.goto(randint(-w, w), randint(-h, h))
+
+def catch(x, y):
+    t.points = t.points + 1
+    t.clear()
+    t.write('A!', font=("Arial", 14, "normal"))
+    rand_move()
+
+t.onclick(catch)
+
+rand_move()
+
+while t.points < 5:
+    sleep(3)
+    rand_move()
+
+t.write("WOW!", font=("Arial", 16, "bold"))
+t.hideturtle()
+
+done()
